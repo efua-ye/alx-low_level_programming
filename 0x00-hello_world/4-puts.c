5042f49d2a4f4a748471413b51f413e984013fa9
efua-ye <efuayegodwin@gmail.com>
2022-10-17 04:55:27 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * My first C PROGRAM
+ * Always begin with a header file
+ * Then your main function
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
